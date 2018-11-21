d9662e2e0e16b055a12674a003b228652974966a
Sarit Ritwirune <sarit@elcolie.com>
2018-11-21 17:04:12 +0700

    Study `torch.multinomial` Use it in `PracticalRNN.ipynb` Save trained model
    Signed-off-by: Sarit Ritwirune <sarit@elcolie.com>

--- experiments.py
+++ experiments.py
@@ -1,3 +1,6 @@
+import torch
+
+
 class StudyObject:
     def __init__(self, a: int, b: str):
         self.a = a
@@ -7,9 +10,19 @@ class StudyObject:
         print("I am being called")
 
 
-def main():
+def test():
     ss = StudyObject(88, "Hello")
     ss(1, "cat")
+
+
+def main():
+    output = torch.DoubleTensor([
+        2.4021, 1.1024, 4.2021, 3.5821, 4.6106, 6.0499, 7.3469, 8.0217,
+        8.9718, 10.0243
+    ])
+    prob = output.exp()
+    weights = torch.multinomial(prob, 2)
+    print(weights)
 
 
 if __name__ == '__main__':
